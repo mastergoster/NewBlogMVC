7bec17199164ffe1f6975f874a136266e3a1ef60
mastergoster <dugrais.julien@icloud.com>
2019-06-13 10:45:10 +0200

    tests unitaires config

--- www/tests/hamming.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function distance($a, $b)
-{
-    if (strlen($a) !== strlen($b)) throw new \Exception('DNA strands must be of equal length.');
-    return count(array_diff_assoc(str_split($a), str_split($b)));
-}
--- www/tests/hello-world.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function helloWorld(string $name = "World")
-{
-    return 'Hello, ' . ucfirst(strtolower($name)) . '!';
-}
